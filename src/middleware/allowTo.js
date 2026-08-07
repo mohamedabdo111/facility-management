38199c824e01b9560c6e64eb72ea56a83d36ed65
mohamedabdo111 <moabdo346@gmail.com>
2026-08-07 17:48:02 +0300

    Enhance role validation in allowTo middleware for case insensitivity
    - Introduced normalization of roles to ensure case-insensitive checks in the allowTo middleware, improving authorization accuracy.
    - Simplified the role validation logic for better clarity and maintainability.

--- src/middleware/allowTo.js
+++ src/middleware/allowTo.js
@@ -1,6 +1,7 @@
 export const allowTo = (...roles) => {
+  const normalizedRoles = roles.map((role) => role.toLowerCase());
   return (req, res, next) => {
-    if (!roles.includes(req.user?.role.toLowerCase())) {
+    if (!normalizedRoles.includes(req.user?.role.toLowerCase())) {
       return res.status(403).json({ message: "You are not authorized to access this resource" });
     }
     next();
