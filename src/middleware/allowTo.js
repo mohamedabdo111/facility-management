29eeea52c1f0312086403f1db03dd1fddf229e15
mohamedabdo111 <moabdo346@gmail.com>
2026-08-07 17:34:56 +0300

    Fix role validation logic in allowTo middleware for accurate authorization checks
    - Updated the role comparison in the allowTo middleware to correctly check if the user's role is included in the provided roles array, ensuring proper authorization.
    - Enhanced code clarity by simplifying the validation condition.

--- src/middleware/allowTo.js
+++ src/middleware/allowTo.js
@@ -1,6 +1,6 @@
 export const allowTo = (...roles) => {
   return (req, res, next) => {
-    if (!roles.toLowerCase().includes(req.user?.role.toLowerCase())) {
+    if (!roles.includes(req.user?.role.toLowerCase())) {
       return res.status(403).json({ message: "You are not authorized to access this resource" });
     }
     next();
